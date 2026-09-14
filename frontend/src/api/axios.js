f6cda3b0c5a0eebecb7f5df6e1dba6811679df2e
Atul_khadse <khadseatul078@gmail.com>
2026-09-14 11:43:19 +0530

    fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api/v1' || 'https://url-backend-mljr.onrender.com',
-   baseURL: $`${import.meta.env.VITE_API_BASE_URL}/api/v1` || 'https://url-backend-mljr.onrender.com',
+   baseURL: import.meta.env.VITE_API_BASE_URL || 'https://url-backend-mljr.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
