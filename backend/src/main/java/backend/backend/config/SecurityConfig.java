f75e8f7fa62a5cdaa2ba8adfb3ca9ab7bdfb2aa8
Atul_khadse <khadseatul078@gmail.com>
2026-09-14 10:34:17 +0530

    cores allow

--- backend/src/main/java/backend/backend/config/SecurityConfig.java
+++ backend/src/main/java/backend/backend/config/SecurityConfig.java
@@ -49,8 +49,13 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("*"));
-//        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
+        // config.setAllowedOriginPatterns(List.of("*"));
+       config.setAllowedOrigins(List.of(
+                "http://localhost:5173", 
+                "http://localhost:3000",
+                "https://vercel.app",
+                "https://url-shortener-bice-iota.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
