7f67a93aa503bdc704ebfc89f21df06189759e60
Atul_khadse <khadseatul078@gmail.com>
2026-09-14 11:17:39 +0530

    fix: adjust axios base url fallback path logic

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api/v1' || 'https://url-backend-mljr.onrender.com',
+  // baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api/v1' || 'https://url-backend-mljr.onrender.com',
+   baseURL: import.meta.env.VITE_API_BASE_URL || 'https://onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
